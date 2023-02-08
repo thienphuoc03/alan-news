8160313b735b76e18412d7adc73ba0c0b7211692
thienphuoc <thienphuoc2391@gmail.com>
2023-02-09 00:20:29 +0700

    remove tailwind css

--- alan-news-client/src/main.js
+++ alan-news-client/src/main.js
@@ -1,14 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import './assets/styles/app.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
 
 /* import font awesome icon component */
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 
-import router from './router'
+import router from './router';
 
-loadFonts()
+loadFonts();
 
-createApp(App).use(router).use(vuetify).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app');
